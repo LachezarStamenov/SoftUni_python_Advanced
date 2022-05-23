c5e415a8fcf3330553aaa63ba6cf98279d963933
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-05-23 13:39:43 +0300

    recursive power task loaded

--- 09_Functions_Advanced_Lab/05_recursive_power.py
+++ 09_Functions_Advanced_Lab/05_recursive_power.py
@@ -1,3 +1,12 @@
 # Create a recursive function called recursive_power() which should receive a number and a power. Using recursion,
 # return the result of number ** power. Submit only the function in the judge system.
 
+def recursive_power(number: int, power: int):
+    output = number
+    if power > 1:
+        output *= recursive_power(number, power - 1)
+
+    return output
+
+
+print(recursive_power(2, 10))
