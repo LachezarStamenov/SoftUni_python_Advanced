fa6f19888e3012f38ebac2eb5b3d8dbf309890c6
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-06-02 22:51:46 +0300

    pawn wars task loaded

--- Exam/02_pawn_wars.py
+++ Exam/02_pawn_wars.py
@@ -29,36 +29,86 @@
 # •	There will be no case where two pawns are placed on the same column.
 # •	There will be no case where black/white will be placed on the last rank.
 
-def check_if_can_capture(row_w, col_w, row_b, col_b):
-    if row_w == row_b - 1 and col_w == col_b - 1:
-        return
+def check_if_can_capture(coordinates_attacker, coordinates_defender):
+    row_a = coordinates_attacker[0]
+    col_a = coordinates_attacker[1]
+    row_d = coordinates_defender[0]
+    col_d = coordinates_defender[1]
+    if row_a - 1 >= 0 and col_a - 1 >= 0:
+        if row_a - 1 == row_d and col_a - 1 == col_d:
+            return [row_a - 1, col_a - 1]
+    if row_a - 1 >= 0 and col_a + 1 < 8:
+        if row_a - 1 == row_d and col_a + 1 == col_d:
+            return [row_a - 1, col_a + 1]
+    if row_a + 1 < 8 and col - 1 >= 0:
+        if row_a + 1 == row_d and col_a - 1 == col_d:
+            return [row_a + 1, col_a - 1]
+    if row_a + 1 < 8 and col + 1 < 8:
+        if row_a + 1 == row_d and col_a + 1 == col_d:
+            return [row_a + 1, col_a + 1]
 
 
-size = 8
 matrix = []
+for _ in range(8):
+    matrix.append(input().split())
 
+white_pawn_coordinates = []
+black_pawn_coordinates = []
 
-player_w_row = 0
-player_w_col = 0
-player_b_row = 0
-player_b_col = 0
+position_row = {
+    0: "8",
+    1: "7",
+    2: "6",
+    3: "5",
+    4: "4",
+    5: "3",
+    6: "2",
+    7: "1",
+}
+positions_col = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h",
+}
 
-for row in range(size):
-    row_elements = input().split()
-    matrix.append(row_elements)
-
-    for col in range(size):
-        if row_elements[col] == 'w':
-            player_w_row = row
-            player_w_col = col
-        elif row_elements[col] == 'b':
-            player_b_row = row
-            player_b_col = col
+for row in range(8):
+    for col in range(8):
+        if matrix[row][col] == "w":
+            white_pawn_coordinates = [row, col]
+        if matrix[row][col] == "b":
+            black_pawn_coordinates = [row, col]
 
 for _ in range(8):
+    capture_on = check_if_can_capture(white_pawn_coordinates, black_pawn_coordinates)
+    if capture_on:
+        position = positions_col[capture_on[1]] + position_row[capture_on[0]]
+        print(f"Game over! White win, capture on {position}.")
+        break
 
+    white_pawn_coordinates[0] -= 1
 
+    if white_pawn_coordinates[0] == 0:
+        position = positions_col[white_pawn_coordinates[1]] + position_row[white_pawn_coordinates[0]]
+        print(f"Game over! White pawn is promoted to a queen at {position}.")
+        break
 
+    capture_on = check_if_can_capture(black_pawn_coordinates, white_pawn_coordinates)
+    if capture_on:
+        position = positions_col[capture_on[1]] + position_row[capture_on[0]]
+        print(f"Game over! Black win, capture on {position}.")
+        break
+
+    black_pawn_coordinates[0] += 1
+
+    if black_pawn_coordinates[0] == 7:
+        position = positions_col[black_pawn_coordinates[1]] + position_row[black_pawn_coordinates[0]]
+        print(f"Game over! Black pawn is promoted to a queen at {position}.")
+        break
 
 
 
