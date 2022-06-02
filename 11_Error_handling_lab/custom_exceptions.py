3b62c83b0622b33a6a399834fab1e725fa523bf2
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-06-02 20:38:03 +0300

    custom exceptions loaded

---
+++ 11_Error_handling_lab/custom_exceptions.py
@@ -0,0 +1,18 @@
+# class ValueTooSmallException(Exception):
+#     pass
+#
+#
+# value = int(input())
+#
+# if value < 10:
+#     raise ValueTooSmallException(f'{value} should be equal or greater than 10')
+
+class ValueCannotBeNegative(Exception):
+    pass
+
+
+values = [int(input()) for _ in range(5)]
+
+for value in values:
+    if value < 0:
+        raise ValueCannotBeNegative
