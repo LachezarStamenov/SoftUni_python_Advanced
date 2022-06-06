b5e86676dddb900d90f9fb9f1783cf65a0fc63fd
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-06-06 15:47:13 +0300

    File handling - even lines loaded

---
+++ 14_File_handling_exercises/01_even_lines/01_even_lines.py
@@ -0,0 +1,12 @@
+# Write a program that reads a text file and prints on the console its even lines. Line numbers start from 0. Before you
+# print the result, replace {"-", ",", ".", "!", "?"} with "@" and reverse the order of the words.
+from fileinput import filename
+chars_to_replace = ["-", ",", ".", "!", "?"]
+
+with open('text.txt', 'r') as file:
+    for row, line in enumerate(file):
+        if row % 2 == 0:
+            result = ' '.join(reversed(line.strip().split()))
+            for char in chars_to_replace:
+                result = result.replace(char, '@')
+            print(result)
