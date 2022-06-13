4007d3a57094cd16408d16d3898991aa98736a51
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-06-13 21:10:58 +0300

    updates

--- 16_Modules_lab/02_ASCII_art.py
+++ 16_Modules_lab/02_ASCII_art.py
@@ -1,17 +1,18 @@
 # Write a program that encrypts given words by using the characters: "-|_/\()" to structure the word. Use the pyfiglet
-# module. You can read more about it here - https://www.geeksforgeeks.org/python-ascii-art-using-pyfiglet-module/
+# module.
 # Directions
 # 1.	First you need to install the module that we will be using. To install it go to Setting --> Project
 # <your_project_name> --> Project Interpreter --> + --> search for pyfiglet --> install package.
 # 2.	Import the module
 # 3.	Implement the logic. We will be using the figlet_format method
 from pyfiglet import figlet_format
-
+from termcolor import colored
 
 def print_art(msg):
-    ascii_art = figlet_format(msg)
+    ascii_art = figlet_format(msg, font="isometric1")
     print(ascii_art)
 
 
 message = input()
 print_art(message)
+print(colored(message, 'green'))
---
+++ triangle/03_triangle.py
@@ -0,0 +1,6 @@
+from triangle.line import print_triangle
+
+size = int(input())
+print(print_triangle(size))
+
+      
